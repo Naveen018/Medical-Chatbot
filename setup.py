dea147dca519e967b8f0d55f6393563d74f45ea1
Naveen V <naveenv3112000@gmail.com>
2025-03-17 23:33:43 +0530

    Project directory structure created

---
+++ setup.py
@@ -0,0 +1,11 @@
+from gettext import install
+from setuptools import setup, find_packages
+
+setup(
+    name='Medical Chatbot GenAI Project',
+    version='0.1.0',
+    author='Naveen',
+    author_email='naveenv3112000@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
